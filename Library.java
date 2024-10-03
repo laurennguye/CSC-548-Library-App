4c215b90afa3a55f180f737b414ad6901cdb346a
laurennguye <157629652+laurennguye@users.noreply.github.com>
2024-10-02 20:42:29 -0500

    Create Library.java

---
+++ Library.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<Book> books;
+
+    public Library() {
+        books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public List<Book> viewBooks() {
+        return books;
+    }
+
+    public Book searchBookByTitle(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                return book;
+            }
+        }
+        return null;
+    }
+}
